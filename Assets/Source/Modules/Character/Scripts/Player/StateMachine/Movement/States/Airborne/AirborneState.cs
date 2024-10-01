1692e8d9f16c29c590fef6c3d11842a6f3cbb174
uriy <78085017+monstebit@users.noreply.github.com>
2024-10-01 10:03:34 +0300

    refactor movement

--- Assets/Source/Modules/Character/Scripts/Player/StateMachine/Movement/States/Airborne/AirborneState.cs
+++ Assets/Source/Modules/Character/Scripts/Player/StateMachine/Movement/States/Airborne/AirborneState.cs
@@ -1,5 +1,4 @@
 using Source.Modules.Character.Scripts.Player.StateMachine.Interfaces;
-using Source.Modules.Character.Scripts.Player.StateMachine.Movement.States.Grounded.StaticAction;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
