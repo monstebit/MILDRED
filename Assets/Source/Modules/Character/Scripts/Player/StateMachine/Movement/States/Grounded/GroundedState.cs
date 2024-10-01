ca3acf06e3510868ef71abad46ac6cbf1dc3e829
uriy <78085017+monstebit@users.noreply.github.com>
2024-10-01 11:20:54 +0300

    refactor ground state

--- Assets/Source/Modules/Character/Scripts/Player/StateMachine/Movement/States/Grounded/GroundedState.cs
+++ Assets/Source/Modules/Character/Scripts/Player/StateMachine/Movement/States/Grounded/GroundedState.cs
@@ -1,7 +1,5 @@
 using Source.Modules.Character.Scripts.Player.StateMachine.Interfaces;
 using Source.Modules.Character.Scripts.Player.StateMachine.Movement.States.Airborne;
-using Source.Modules.Character.Scripts.Player.StateMachine.Movement.States.Grounded.Moving;
-using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace Source.Modules.Character.Scripts.Player.StateMachine.Movement.States.Grounded
@@ -9,7 +7,6 @@ namespace Source.Modules.Character.Scripts.Player.StateMachine.Movement.States.G
     public abstract class GroundedState : MovementState
     {
         private readonly PlayerCompositionRoot _playerCompositionRoot;
-        private readonly PlayerConfig _playerConfig;
 
         protected GroundedState(
             IStateSwitcher stateSwitcher,
@@ -20,31 +17,13 @@ namespace Source.Modules.Character.Scripts.Player.StateMachine.Movement.States.G
             data)
         {
             _playerCompositionRoot = playerCompositionRoot;
-            _playerConfig = playerCompositionRoot.PlayerConfig;
         }
         
         public override void Enter()
         {
             base.Enter();
-
+            
             PlayerView.StartGrounded();
-
-            UpdateShouldSprintState();  //  FOW WHAT?
-        }
-        
-        private void UpdateShouldSprintState()
-        {
-            if (_playerConfig.MovementStateConfig.ShouldSprint == false)
-            {
-                return;
-            }
-            
-            if (Data.MovementInput != Vector2.zero)
-            {
-                return;
-            }
-            
-            _playerConfig.MovementStateConfig.ShouldSprint = false;
         }
         
         public override void Exit()
@@ -63,16 +42,13 @@ namespace Source.Modules.Character.Scripts.Player.StateMachine.Movement.States.G
         
         protected virtual void CheckGroundAndSwitchToFalling()
         {
-            // Проверка, касаемся ли земли
             if (_playerCompositionRoot.GroundChecker.isTouches == false)
             {
-                // Если идёт анимационный переход, выходим из метода
                 if (InAnimationTransition())
                 {
                     return;
                 }
 
-                // Переход в состояние падения
                 StateSwitcher.SwitchState<FallingState>();
             }
         }
@@ -94,11 +70,6 @@ namespace Source.Modules.Character.Scripts.Player.StateMachine.Movement.States.G
         protected virtual void OnJumpStarted(InputAction.CallbackContext context)
         {
             StateSwitcher.SwitchState<JumpingState>();
-        }
-        
-        protected virtual bool InAnimationTransition(int layerIndex = 0)
-        {
-            return _playerCompositionRoot.PlayerView.Animator.IsInTransition(layerIndex);
         }
     }
 }
