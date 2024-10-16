a6aa5ba615dfb0774492c0985d77657a0c69b6ee
uriy <78085017+monstebit@users.noreply.github.com>
2024-10-16 22:16:53 +0300

    move RPC methods to PNS class from PlayerView

--- Assets/Source/Modules/Character/Scripts/Player/PlayerView.cs
+++ Assets/Source/Modules/Character/Scripts/Player/PlayerView.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections.Generic;
-using TMPro;
 using Unity.Netcode;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -10,28 +8,44 @@ namespace Source.Modules.Character.Scripts.Player
     [RequireComponent(typeof(Animator))]
     public class PlayerView : NetworkBehaviour
     {
-        // public void Initialize() => Animator = GetComponent<Animator>();
+        private readonly HashSet<string> _activeStates = new();
+        
         [SerializeField] private PlayerCompositionRoot _playerCompositionRoot;
         [SerializeField] private Vector2 _defaultInitialPosition = new Vector2(-4, 4);
+        PlayerNetworkSynchronizer _playerNetworkSynchronizer;
+        
         [SerializeField] public Animator Animator;
+
+        private void Awake()
+        {
+            _playerNetworkSynchronizer = _playerCompositionRoot.PlayerNetworkSynchronizer;
+        }
+
+        private void Start()
+        {
+            InitializePlayerPosition();
+        }
+
+        private void Update()
+        {
+            UpdateAnimatorMovementParameters();
+        }
+
+        private void InitializePlayerPosition()
+        {
+            // if (IsClient && IsOwner)
+            // {
+            //     transform.position = new Vector3(Random.Range(
+            //             _defaultInitialPosition.x, _defaultInitialPosition.y), 0,
+            //         Random.Range(_defaultInitialPosition.x, _defaultInitialPosition.y));
+            // }
+            transform.position = new Vector3(Random.Range(
+                    _defaultInitialPosition.x, _defaultInitialPosition.y), 0,
+                Random.Range(_defaultInitialPosition.x, _defaultInitialPosition.y));
+        }
         
-        private HashSet<string> _activeStates = new();
-        
-        // //  ON TESTING
-        // private void Start()
-        // {
-        //     if (IsClient && IsOwner)
-        //     {
-        //         transform.position = new Vector3(Random.Range(
-        //                 _defaultInitialPosition.x, _defaultInitialPosition.y), 0,
-        //             Random.Range(_defaultInitialPosition.x, _defaultInitialPosition.y));
-        //     }
-        // }
-        // //  ON TESTING
-        
-        private bool IsOwner => _playerCompositionRoot.PlayerNetworkSynchronizer.IsOwner; 
-        
-        public void UpdateAnimatorMovementParameters()
+        //  PLAYER ANIMATION UPDATERS
+        private void UpdateAnimatorMovementParameters()
         {
             int vertical = Animator.StringToHash("Vertical");
             int horizontal = Animator.StringToHash("Horizontal");
@@ -45,42 +59,21 @@ namespace Source.Modules.Character.Scripts.Player
         
         public void UpdateState(string stateName, bool newState)
         {
-            if (IsOwner)
-            {
-                bool isStateActive = _activeStates.Contains(stateName);
+            if (IsOwner == false) return;
+            
+            bool isStateActive = _activeStates.Contains(stateName);
 
-                // Если новое состояние отличается от текущего
-                if (newState && !isStateActive)
-                {
-                    _activeStates.Add(stateName); // Добавляем активное состояние
-                    UpdateAnimationStateServerRpc(
-                        stateName, true);
-                }
-                else if (!newState && isStateActive)
-                {
-                    _activeStates.Remove(stateName); // Убираем неактивное состояние
-                    UpdateAnimationStateServerRpc(
-                        stateName, false);
-                }
+            // Если новое состояние отличается от текущего
+            if (newState && !isStateActive)
+            {
+                _activeStates.Add(stateName); // Добавляем активное состояние
+                _playerNetworkSynchronizer.UpdateAnimationStateServerRpc(stateName, true);
+            }
+            else if (!newState && isStateActive)
+            {
+                _activeStates.Remove(stateName); // Убираем неактивное состояние
+                _playerNetworkSynchronizer.UpdateAnimationStateServerRpc(stateName, false);
             }
         }
-        
-        //  NETWORK
-        [ServerRpc]
-        public void UpdateAnimationStateServerRpc(string stateName, bool state)
-        {
-            // Если это сервер, то инициируем обновление состояния на клиентах
-            if (IsServer)
-            {
-                SyncAnimationStateClientRpc(stateName, state);
-            }
-        }
-
-        [ClientRpc]
-        private void SyncAnimationStateClientRpc(string stateName, bool state)
-        {
-            _playerCompositionRoot.PlayerView.Animator.SetBool(stateName, state);
-        }
-        //  NETWORK
     }
 }
