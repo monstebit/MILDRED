0a9c39b7c4dd985151207aa26d04a9963a544639
uriy <78085017+monstebit@users.noreply.github.com>
2024-10-16 22:14:59 +0300

    clear CharactrerNetworkSync class

--- Assets/Source/Modules/Character/Scripts/CharacterNetworkSynchronizer.cs
+++ Assets/Source/Modules/Character/Scripts/CharacterNetworkSynchronizer.cs
@@ -1,33 +1,8 @@
 using Unity.Netcode;
-using UnityEngine;
 
 namespace Source.Modules.Character.Scripts
 {
     public class CharacterNetworkSynchronizer : NetworkBehaviour
     {
-        [Header("Position")] 
-        public NetworkVariable<Vector3> NetworkPosition = new NetworkVariable<Vector3>(
-            Vector3.zero,
-            NetworkVariableReadPermission.Everyone,
-            NetworkVariableWritePermission.Owner);
-
-        public NetworkVariable<Quaternion> NetworkRotation = new NetworkVariable<Quaternion>(
-            Quaternion.identity,
-            NetworkVariableReadPermission.Everyone,
-            NetworkVariableWritePermission.Owner);
-        
-        public Vector3 NetworkPositionVelocity;
-        public float NetworkPositionSmoothTime = 0.1f;
-        public float NetworkRotationSmoothTime = 0.1f;
-        
-        [Header("Animator")]
-        public NetworkVariable<float> HorizontalMovement = 
-            new NetworkVariable<float>(0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
-        
-        public NetworkVariable<float> VerticalMovement = 
-            new NetworkVariable<float>(0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
-        
-        public NetworkVariable<float> MoveAmount = 
-            new NetworkVariable<float>(0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
     }
 }
